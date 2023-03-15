26e1bbc042c9867d25b2bd9bf8a7f001eb16058e
Abdirizak Mohamed <abdirizakm123@gmail.com>
2023-03-15 11:52:52 +0300

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,4 +9,9 @@ class StudentsController < ApplicationController
         students = Student.order("grade DESC")
         render json: students
     end
+
+    def highest_grade
+        student = Student.order("grade DESC").first
+        render json: student
+    end
 end
